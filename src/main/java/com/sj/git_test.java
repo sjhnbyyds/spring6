6fdc90d9b0ac0d9ace7317de7f2667f12b20fb63
sj <2300068020@qq.com>
2023-04-02 16:52:38 +0800

    third commit

--- src/main/java/com/sj/git_test.java
+++ src/main/java/com/sj/git_test.java
@@ -4,6 +4,8 @@ public class git_test {
     public static void main(String[] args) {
         System.out.println("hello git!");
         System.out.println("hello git2!");
+        System.out.println("hello git3!");
+
 
     }
 }
